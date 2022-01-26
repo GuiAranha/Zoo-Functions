feec49c542e737fd26bf32705b1b8418b834f044
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-01-26 11:57:03 -0300

    Requisito 5 - ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui.
+  if (animal === undefined) {
+    return data.species.reduce((acc, curr) => ({ ...acc, [curr.name]: curr.residents.length }), {});
+  }
+  if (animal.sex === undefined) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return data.species.find((specie) => specie.name === animal.specie).residents
+    .filter((specie) => specie.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
