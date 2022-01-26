645d441a0437cdd4002f5770cb2c8776c1abe0bd
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-01-26 14:51:12 -0300

    Requisito 6 - ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const children = entrants.filter((person) => person.age < 18).length;
+  const adults = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const seniors = entrants.filter((person) => person.age >= 50).length;
+  return {
+    child: children,
+    adult: adults,
+    senior: seniors,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+
+  return (count.child * data.prices.child)
+    + (count.adult * data.prices.adult)
+    + (count.senior * data.prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
