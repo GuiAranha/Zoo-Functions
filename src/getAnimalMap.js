48144134a21b4be51256d62447a2fa4192834daa
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-01-27 17:20:08 -0300

    Requisito 7 - ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,68 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+/* const find = (location) => data.species.filter((specie) => specie.location === location)
+  .map((specie) => specie.name); */
+const locations = ['NE', 'NW', 'SE', 'SW'];
+const getSpecies = (location) => data.species.filter((specie) => specie.location === location);
+const animals = locations.map((location) => getSpecies(location));
+
+const getAnimalsByRegion = () => animals.reduce((acum, species, index) => (
+  { ...acum, [locations[index]]: species.map((specie) => specie.name) }), {});
+
+const getAnimalsNames = () => animals.reduce((acum, species, ind) => (
+  { ...acum,
+    [locations[ind]]: species.map((specie) => ({
+      [specie.name]: specie.residents.map((animal) => animal.name) })),
+  }), {});
+
+const getAnimalsNamesSex = (sex) => animals.reduce((acum, species, ind) => (
+  { ...acum,
+    [locations[ind]]: species.map((specie) => ({
+      [specie.name]: specie.residents.filter((animal) => animal.sex === sex)
+        .map((animal) => animal.name) })),
+  }), {});
+
+const getAnimalsNamesSexSort = (sex) => animals.reduce((acum, species, ind) => (
+  { ...acum,
+    [locations[ind]]: species.map((specie) => ({
+      [specie.name]: specie.residents.filter((animal) => animal.sex === sex)
+        .map((animal) => animal.name).sort() })),
+  }), {});
+
+const getAnimalsNamesSort = () => animals.reduce((acum, species, ind) => (
+  { ...acum,
+    [locations[ind]]: species.map((specie) => ({
+      [specie.name]: specie.residents.map((animal) => animal.name).sort() })),
+  }), {});
+
+function sexAndSorted(options) {
+  if (!options.sex) {
+    if (!options.sorted) {
+      return getAnimalsNames();
+    }
+    return getAnimalsNamesSort();
+  }
+  if (!options.sorted) {
+    return getAnimalsNamesSex(options.sex);
+  }
+  return getAnimalsNamesSexSort(options.sex);
 }
+
+function getAnimalMap(options) {
+  if (options === undefined || !options.includeNames) {
+    return getAnimalsByRegion();
+  }
+  return sexAndSorted(options);
+}
+  /* 
+  if (options.includeNames === true) {
+    if (options.sorted === true) {
+    return getAnimalsNamesSort();
+    }
+    return getAnimalsNames();
+  }
+  if (!options.sex) {
+    return getAnimalsNamesSex(options.sex);
+  } */
 
 module.exports = getAnimalMap;
